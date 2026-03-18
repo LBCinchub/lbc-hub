44b182a44f5c2b997c204909d37a20c0c721782d
Mokhtar tarek Samara <mokhtartareksamara@gmail.com>
2026-03-18 06:58:09 +0000

    File changes

--- functions/botActivity.ts
+++ functions/botActivity.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 const BOTS = [
   { name: "Alex Rivera", email: "alex.rivera.bot@lbchub.ai", avatar: "https://i.pravatar.cc/150?img=1" },
